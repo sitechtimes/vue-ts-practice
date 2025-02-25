51135003ccfa9e100b36fee982807ba682ee5343
Kenneth Ng <145921389+kennething@users.noreply.github.com>
2025-02-24 23:28:19 -0500

    Update variables2.vue

--- pages/01_variables/variables2.vue
+++ pages/01_variables/variables2.vue
@@ -9,7 +9,6 @@ onMounted(async () => {
   // TODO: refactor the below code to make `serverIsOnline` a `const` variable instead of `let`
   let serverIsOnline: boolean;
 
-  // TODO: optional challenge - refactor this if/else into a 1 liner
   if (serverStatus.status === "online") {
     serverIsOnline = true;
   } else {
